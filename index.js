0a92abecb30ca4f6039d7e4b92e54505f69df5f2
Darpan Sudan <darpansudan99@gmail.com>
2023-11-22 15:44:08 +0530

    new

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const { Stripe } = require("stripe");
 const dotenv = require("dotenv").config();
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+  //   origin: ["http://localhost:3000"],
+  origin: "*",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 8080;
@@ -74,7 +79,7 @@ app.post("/login", async (req, res) => {
         email: user.email,
         image: user.image,
       };
-     // console.log(dataSend);
+      // console.log(dataSend);
       res.send({
         message: "Login is successful",
         alert: true,
@@ -98,78 +103,70 @@ app.post("/login", async (req, res) => {
 //product section
 const schemaProduct = mongoose.Schema({
   name: String,
-  category:String,
+  category: String,
   image: String,
   price: String,
   description: String,
 });
-const productModel = mongoose.model("product",schemaProduct)
+const productModel = mongoose.model("product", schemaProduct);
 
-//save product in data 
+//save product in data
 //api
-app.post("/uploadProduct",async(req,res)=>{
+app.post("/uploadProduct", async (req, res) => {
   // console.log(req.body)
-  const data = await productModel(req.body)
-  const datasave = await data.save()
-  res.send({message : "Upload successfully"})
-})
+  const data = await productModel(req.body);
+  const datasave = await data.save();
+  res.send({ message: "Upload successfully" });
+});
 
-
-app.get("/product",async(req,res)=>{
-const data = await productModel.find({})
-res.send(JSON.stringify(data))
-})
+app.get("/product", async (req, res) => {
+  const data = await productModel.find({});
+  res.send(JSON.stringify(data));
+});
 
 /*****payment getWay */
-console.log(process.env.STRIPE_SECRET_KEY)
+console.log(process.env.STRIPE_SECRET_KEY);
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-const stripe  = new Stripe(process.env.STRIPE_SECRET_KEY)
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const params = {
+      submit_type: "pay",
+      mode: "payment",
+      payment_method_types: ["card"],
+      billing_address_collection: "auto",
+      shipping_options: [{ shipping_rate: "shr_1OD0sDSBiTGeTWlnOtZi5bDZ" }],
 
-app.post("/create-checkout-session",async(req,res)=>{
+      line_items: req.body.map((item) => {
+        return {
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: item.name,
+              // images : [item.image]
+            },
+            unit_amount: item.price * 100,
+          },
+          adjustable_quantity: {
+            enabled: true,
+            minimum: 1,
+          },
+          quantity: item.qty,
+        };
+      }),
 
-     try{
-      const params = {
-          submit_type : 'pay',
-          mode : "payment",
-          payment_method_types : ['card'],
-          billing_address_collection : "auto",
-          shipping_options : [{shipping_rate : "shr_1OD0sDSBiTGeTWlnOtZi5bDZ"}],
+      success_url: `${process.env.FRONTEND_URL}/success`,
+      cancel_url: `${process.env.FRONTEND_URL}/cancel`,
+    };
 
-          line_items : req.body.map((item)=>{
-            return{
-              price_data : {
-                currency : "inr",
-                product_data : {
-                  name : item.name,
-                  // images : [item.image]
-                },
-                unit_amount : item.price * 100,
-              },
-              adjustable_quantity : {
-                enabled : true,
-                minimum : 1,
-              },
-              quantity : item.qty
-            }
-          }),
-
-          success_url : `${process.env.FRONTEND_URL}/success`,
-          cancel_url : `${process.env.FRONTEND_URL}/cancel`,
-
-      }
-
-      
-      const session = await stripe.checkout.sessions.create(params)
-      // console.log(session)
-      res.status(200).json(session.id)
-     }
-     catch (err){
-        res.status(err.statusCode || 500).json(err.message)
-     }
-
-})
-
+    const session = await stripe.checkout.sessions.create(params);
+    // console.log(session)
+    res.status(200).json(session.id);
+  } catch (err) {
+    res.status(err.statusCode || 500).json(err.message);
+  }
+});
 
 //server is running
 app.listen(PORT, () => console.log("server is running at port :" + PORT));
