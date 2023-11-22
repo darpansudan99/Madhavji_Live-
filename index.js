b24607d3552c230bd3872cb745037cbcec594e86
Darpan Sudan <darpansudan99@gmail.com>
2023-11-22 17:16:27 +0530

    new

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE");
+  res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "X-Requested-With, Access-Control-Allow-Headers, Content-Type, Authorization, Origin, Accept"
   );
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 
